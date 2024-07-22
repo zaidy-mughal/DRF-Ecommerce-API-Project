27425002240f331848401188923890c869ca2df9
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2024-07-23 00:07:47 +0500

    Written Some Views

--- product_app/views.py
+++ product_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Category, Brand
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
-# Create your views here.
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
